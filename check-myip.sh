f410ff1b80ad3cebdc62bfa8682dbeceffd5c792
lynix757 <pornsak.backup@gmail.com>
2020-03-16 13:21:57 +0700

    Update check-myip.sh

--- check-myip.sh
+++ check-myip.sh
@@ -1,1 +1,3 @@
-dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}'
+myip=$(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}')
+echo $myip
+whois $myip | grep netname | awk {'print $2'}
